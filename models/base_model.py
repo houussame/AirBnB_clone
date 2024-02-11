792793ec2b6f452d1c47607af241321db464883c
houussame <2015.houdou@gmail.com>
2024-02-11 11:55:24 -0800

    b

--- models/base_model.py
+++ models/base_model.py
@@ -5,28 +5,26 @@ from datetime import datetime
 import models
 
 
-class BaseModel:
+class BaseModel():
     '''it s a class parent'''
 
     def __init__(self, *args, **kwargs):
         '''creat new base'''
-        if kwargs == 0:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            for (k, v) in kwargs.items():
-                if k == "created_at" or v == "update_at":
-                    self.__dict__[k] = datetime.strptime(v,
+        '''if kwargs == 0:'''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        for (k, v) in kwargs.items():
+            if k == "created_at" or v == "update_at":
+                self.__dict__[k] = datetime.strptime(v,
                                      "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[k] = v
+            else:
+                self.__dict__[k] = v
 
     def __str__(self):
         '''the string representation of an instance'''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                        self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__,self.id, self.__dict__))
 
     def save(self):
         '''update the update_at'''
